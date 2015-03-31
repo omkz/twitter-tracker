1f23a04f6d62fe38dfdf925d10fec87fe48ee545
kurnia muhamad <omenkzz@gmail.com>
2015-03-31 20:30:07 +0700

    test rufus-scheduler on heroku

--- config/initializers/scheduler.rb
+++ config/initializers/scheduler.rb
@@ -4,9 +4,14 @@ s = Rufus::Scheduler.singleton
 
 s.every '5s' do
   # puts "Fetch tweets ..."
-  t = TweetsController.new
-  t.fetch_tweets
+  # t = TweetsController.new
+  # t.fetch_tweets
   # puts "done."
+
+  s.every '5s' do
+    Rails.logger.info "hello, it's #{Time.now}"
+    puts "hello, it's #{Time.now}"
+  end
 end
 
 
