84e7f5d8e5a38229dac04ce6e0b193808c3585f0
kurnia muhamad <omenkzz@gmail.com>
2015-04-01 20:08:26 +0700

    change cron job time: every day, five minutes after midnight

--- config/initializers/scheduler.rb
+++ config/initializers/scheduler.rb
@@ -2,16 +2,21 @@ require 'rufus-scheduler'
 
 s = Rufus::Scheduler.singleton
 
-s.every '5s' do
-  # puts "Fetch tweets ..."
-  # t = TweetsController.new
-  # t.fetch_tweets
-  # puts "done."
-
-  s.every '5s' do
-    Rails.logger.info "hello, it's #{Time.now}"
-    puts "hello, it's #{Time.now}"
-  end
+# do something every day, five minutes after midnight
+s.cron '5 0 * * *' do
+  puts "Fetch tweets ..."
+  t = TweetsController.new
+  t.fetch_tweets
+  puts "done."
 end
+
+# s.every '5s' do
+#   Rails.logger.info "hello, it's #{Time.now}"g
+#   puts "hello, it's #{Time.now}"
+# end
+
+# scheduler.cron '0 23 * * *', :times => 31 do
+#   # ... do something every day at 23:00 but do it no more than 31 times
+# end
 
 
